0ac05517acc7ef65ec49a4e23c916093c7fb7da8
DriftKingTW <driftkingtw@gmail.com>
2022-05-09 15:47:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TinyPICO-BLE-Marco-Pad-Key-Configuration-Tool
+ Web key configuration tool for TinyPICO BLE Macro Pad
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+
+const test = "fjewoifj";
+</script>
+
+<template>
+  <HelloWorld :msg="test" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
