496bef79bc2a356db7c4e2b8fdbd4f7fba53aa64
DriftKingTW <driftkingtw@gmail.com>
2022-05-12 16:42:19 +0800

    feat: update i18n configs

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createI18n } from "vue-i18n";
-import en from "./locales/en-us.json";
-import zhTW from "./locales/zh-tw.json";
-import zhCN from "./locales/zh-cn.json";
+import en from "./locales/en.json";
+import zhTW from "./locales/zh-TW.json";
+import zhCN from "./locales/zh-CN.json";
 import "./index.css";
 
 const i18n = createI18n({
   globalInjection: true,
   fallbackLocale: "en",
   messages: {
+    en,
     "en-US": en,
+    "en-GB": en,
     "zh-TW": zhTW,
     "zh-CN": zhCN,
   },
