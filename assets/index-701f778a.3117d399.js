38c69633988ac93d482a19c7240a76c571ca097b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-01-05 08:15:53 +0000

    Deploy DriftKingTW/TinyPICO-BLE-Keypad-Key-Configuration-Tool to github.com/DriftKingTW/TinyPICO-BLE-Keypad-Key-Configuration-Tool.git:gh-pages

--- assets/index-701f778a.3117d399.js
+++ assets/index-701f778a.3117d399.js
@@ -1,4 +1,4 @@
-import{y as n,d as a,r as s,_ as r,n as c,s as d,$ as e}from"./connect-3e6b1f54.7931f33e.js";import"./index.65fa563a.js";const l=n`
+import{y as n,d as a,r as s,_ as r,n as c,s as d,$ as e}from"./connect-3e6b1f54.5f9d5684.js";import"./index.69e41f5d.js";const l=n`
   <svg
     version="1.1"
     id="Capa_1"
